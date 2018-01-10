b6c55853329d6c4e24fa02ea6c30c4e2cd3aa40a
indicator34 <aidanfallows@outlook.com>
2018-01-10 18:29:03 +0000

    Add files via upload

---
+++ js/Script.js
@@ -0,0 +1,24 @@
+function RobFunction() {
+    document.getElementById("abouttext").innerHTML = "Roberto";
+	document.getElementById('aboutchange').src='assets/Rob.jpg';
+}
+
+function GarethFunction() {
+    document.getElementById("abouttext").innerHTML = "Gareth";
+document.getElementById('aboutchange').src='assets/Gareth.jpg';
+}
+
+function PeredurFunction() {
+    document.getElementById("abouttext").innerHTML = "Peredur";
+document.getElementById('aboutchange').src='assets/Peredur.jpg';
+}
+
+function KJFunction() {
+    document.getElementById("abouttext").innerHTML = "KJ";
+document.getElementById('aboutchange').src='assets/KJ.jpg';
+}
+
+function DJFunction() {
+    document.getElementById("abouttext").innerHTML = "Hello World!";
+document.getElementById('aboutchange').src='assets/DJ.jpg';
+}
